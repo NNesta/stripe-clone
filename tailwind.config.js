c420cced2b7a5d3d3c1ac84e4d2e07be439b5479
Ngabonziza Nestor <ngabonest@gmail.com>
2022-10-23 18:07:27 +0200

    feat:design of desktop version v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
+      clipPath: {
+        myheader: "polygon(0 0, 0 35%, 86% 0)",
+        myfooter: "polygon(0 61%, 100% 40%, 100% 100%, 0% 100%)"
+    },
       fontSize:{
         phoneBorderRadius: "36px",
     phoneScreenBorderRadius: "33px",
@@ -14,11 +18,18 @@ module.exports = {
       {
         'accentColor': '#635bff',
         'titleColor': '#0a2540',
+        'titleColor-2': '#3a3a3a',
         "textColor": "#425466",
         "backgroundColor": '#f6f9fc',
         'backgroundColorDark':"#0a2540",
+        'dark':"#0a2540",
+        'accentTeal':"#00d4ff",
       }
     },
   },
-  plugins: [],
+  
+    plugins: [
+      require('tailwind-clip-path'),
+    ],
+  
 }
